fb7c4702156c35133fecac9abfb8a8f4ccf0fe12
MiaKPup <32400855+MiaKPup@users.noreply.github.com>
2018-04-09 11:23:25 -0400

    stuff

--- FoodServer.py
+++ FoodServer.py
@@ -1,6 +1,6 @@
 # Food Server
-# Team Mac and Cheese
-#
+# Ideaventions Academy
+# 7th Grade 2017-2018
 
 # imports
 import RPi.GPIO as GPIO
@@ -13,16 +13,20 @@ import time
 
 # Turn on/off times for light
 # Target temperature and humidity (76 F and 70% RH)
-TIME_OFF = 21
-TIME_ON = 6
-TEMP_FAN = 76
+TIME_OFF = 21 # 9 pm at night turn off
+TIME_ON = 6 # 6 am turn on
+TEMP_FAN = 76 # temp F
+# Max T
+# Min T
+# Max RH
+# Min RH
+
 # Setup GPIO (2-27)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(27, GPIO.OUT)
 for i in range(2,21):
     GPIO.setup(i,GPIO.OUT)
-GPIO.setup(21,GPIO.OUT)
-
+GPIO.setup(22,GPIO.IN)  
 
 
 # Main loop
