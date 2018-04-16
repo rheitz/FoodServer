2ca533d56c14af9fbee2575cd42e63df71809632
rheitz <rheitz@ideaventionsacademy.org>
2018-04-16 12:00:53 -0400

    cleanup

--- Project/FoodServer.py
+++ Project/FoodServer.py
@@ -1,50 +1,11 @@
 # Food Server
 # Ideaventions Academy
 # 7th Grade 2017-2018
-
 # imports
-import RPi.GPIO as GPIO
 import time
+import Project.Functions
 
 # variables
-
-
-# functions
-
-def fanOn():
-    GPIO.output(11,GPIO.HIGH)
-def fanOff():
-    GPIO.output(11,GPIO.LOW)
-
-def humidifierFanOn():
-    GPIO.output(12,GPIO.HIGH)
-
-def humidifierFanOff():
-    GPIO.output(12,GPIO.LOW)
-
-def misterOn():
-    GPIO.output(13,GPIO.HIGH)
-
-def misterOff():
-    GPIO.output(13,GPIO.LOW)
-
-def lightsOn():
-    GPIO.output(27,GPIO.HIGH)
-    for i in range(2,11):
-        GPIO.output(i,GPIO.HIGH)
-
-def lightsOff():
-    GPIO.output(27,GPIO.LOW)
-    for i in range(2,11):
-        GPIO.output(i,GPIO.LOW)
-
-def humidifierOn():
-    mister.on()
-    fan.on()
-    
-def humidifierOff():
-    mister.off()
-    fan.off()
 
 # Turn on/off times for light
 # Target temperature and humidity (76 F and 70% RH)
@@ -56,23 +17,20 @@ TIME_OFF = 21 # 9 pm at night turn off
 TIME_ON = 6 # 6 am turn on
 MAX_TEMP = 76 # Max T
 MIN_TEMP = 63 # Min T
-TARGET_TEMP
+TARGET_TEMP = 72
 MAX_RH = 50 # Max RH
 MIN_RH = 17 # Min RH
 TARGET_RH = 40
 
-# Setup GPIO (2-27)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(27,GPIO.OUT)
-for i in range(2,21):
-    GPIO.setup(i,GPIO.OUT)
+
+
 
 # Main loop
 # Get time and see if lights should be on or off
 while True:
     time = time.localtime()
     if LightsAreOn == False and time.tm_hour == TIME_ON:
-        lights.on()
+        Funcions
         LightsAreOn = True
     if LightsAreOn == True and time.tm_hour == TIME_OFF:
         lights.off()
---
+++ Project/Functions.py
@@ -0,0 +1,52 @@
+import RPi.GPIO as GPIO
+
+# Setup GPIO (2-27)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(27,GPIO.OUT)
+for i in range(2,21):
+    GPIO.setup(i,GPIO.OUT)
+
+def fanOn():
+    GPIO.output(11, GPIO.HIGH)
+
+
+def fanOff():
+    GPIO.output(11, GPIO.LOW)
+
+
+def humidifierFanOn():
+    GPIO.output(12, GPIO.HIGH)
+
+
+def humidifierFanOff():
+    GPIO.output(12, GPIO.LOW)
+
+
+def misterOn():
+    GPIO.output(13, GPIO.HIGH)
+
+
+def misterOff():
+    GPIO.output(13, GPIO.LOW)
+
+
+def lightsOn():
+    lights = [27, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    for pins in lights:
+        GPIO.output(pins, GPIO.HIGH)
+
+
+def lightsOff():
+    lights = [27, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    for pins in lights:
+        GPIO.output(pins, GPIO.LOW)
+
+
+def humidifierOn():
+    mister.on()
+    fan.on()
+
+
+def humidifierOff():
+    mister.off()
+    fan.off()
